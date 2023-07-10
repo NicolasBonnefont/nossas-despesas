29e5f4ea86071f179159235da9bec83c758e9ece
Nicolas Bonnefont <nicolas.7l@hotmail.com>
2023-07-10 15:56:28 -0300

    fix adsense

---
+++ src/app/adsense.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import Head from "next/head"
+import Script from "next/script"
+
+function AdSense() {
+
+  return (
+    <Head>
+      <Script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1391481324251546"
+        crossOrigin="anonymous" />
+    </Head>)
+
+}
+
+export default AdSense
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
-import Head from 'next/head'
-import Script from 'next/script'
+import AdSense from './adsense'
 import Provider from './components/Providers/Provider'
 import './globals.css'
 
@@ -12,10 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt-br">
-      <Head>
-      <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1391481324251546"
-     crossorigin="anonymous"/>
-      </Head>
+      <AdSense/>
       <body className='font-Poppins flex flex-1 flex-col bg-gray-900 w-full h-screen '>
         <Provider>
           {children}
