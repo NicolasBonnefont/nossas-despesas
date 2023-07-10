7d522ec7b964c42479466a9c4a0e9ce8016639f0
Nicolas Bonnefont <nicolas.7l@hotmail.com>
2023-07-10 15:49:55 -0300

    ad sense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,23 @@
+import Head from 'next/head'
+import Script from 'next/script'
 import Provider from './components/Providers/Provider'
 import './globals.css'
 
 export const metadata = {
   title: 'Teu Money - Gestão Financeira Pessoal',
   description: 'Sistema simples de gestão de despesas pessoais.',
-  keywords:"gestão financeira pessoal, gestão de gastos, controle de gastos pessoais, meu gastos, entrada e saida, sistema financeiro, controle de gastos"
+  keywords: "gestão financeira pessoal, gestão de gastos, controle de gastos pessoais, meu gastos, entrada e saida, sistema financeiro, controle de gastos"
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt-br">
+      <Head>
+      <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1391481324251546"
+     crossorigin="anonymous"/>
+      </Head>
       <body className='font-Poppins flex flex-1 flex-col bg-gray-900 w-full h-screen '>
-        <Provider>       
+        <Provider>
           {children}
         </Provider>
       </body>
