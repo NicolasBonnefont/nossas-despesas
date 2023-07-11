2dfb8ba77527758eac97aff83df2985612faa66a
Nicolas Bonnefont <nicolas.7l@hotmail.com>
2023-07-11 09:07:34 -0300

    fix

--- src/app/dash/layout.tsx
+++ src/app/dash/layout.tsx
@@ -1,8 +1,10 @@
+import AdSense from "../adsense";
 import Menu from "../components/menu";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
+    <AdSense/>
     <Menu/>
     {children}
     </>
