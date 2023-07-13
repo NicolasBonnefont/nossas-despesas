cab5103a0432ab605c17975441c5198632fab466
Nicolas Bonnefont <nicolas.7l@hotmail.com>
2023-07-13 09:17:13 -0300

    fix adsense

--- src/app/adsense.tsx
+++ src/app/adsense.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import Head from "next/head"
 import Script from "next/script"
 
@@ -12,7 +10,8 @@ function AdSense() {
         strategy="lazyOnload"
         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1391481324251546"
         crossOrigin="anonymous" />
-    </Head>)
+    </Head>
+    )
 
 }
 
