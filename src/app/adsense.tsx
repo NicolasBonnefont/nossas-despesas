39cea83c2d8158bc046c605d8b9e9863bdaeb5ed
Nicolas Bonnefont <nicolas.7l@hotmail.com>
2023-07-13 09:07:48 -0300

    fix google ads

--- src/app/adsense.tsx
+++ src/app/adsense.tsx
@@ -9,6 +9,7 @@ function AdSense() {
     <Head>
       <Script
         async
+        strategy="lazyOnload"
         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1391481324251546"
         crossOrigin="anonymous" />
     </Head>)
