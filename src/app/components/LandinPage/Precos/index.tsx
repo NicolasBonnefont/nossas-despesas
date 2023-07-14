8556ffe967c679b9ef65abb295b5a1ecbc366e20
Nicolas Bonnefont <nicolas.7l@hotmail.com>
2023-07-14 13:41:00 -0300

    add price teu money pro

--- src/app/components/LandinPage/Precos/index.tsx
+++ src/app/components/LandinPage/Precos/index.tsx
@@ -28,30 +28,33 @@ function Precos() {
 
       </div>
 
-      <div className=" flex flex-col bg-white rounded-xl shadow-lg w-80 h-96 hover:bg-gray-200 cursor-pointer " >
+      <a target='blank' href="https://buy.stripe.com/eVa3g9dNIdkjguc5km">
+        <div className=" flex flex-col bg-white rounded-xl shadow-lg w-80 h-96 hover:bg-gray-200 cursor-pointer " >
 
-        <div className=" flex items-center justify-center w-full h-10 bg-blue-900 rounded-b-xl rounded-t-lg">
-          <p className="text-yellow-200 font-bold">Melhor experiência</p>
+          <div className=" flex items-center justify-center w-full h-10 bg-blue-900 rounded-b-xl rounded-t-lg">
+            <p className="text-yellow-200 font-bold">Melhor experiência</p>
+          </div>
+
+          <div className=" mt-14 flex flex-col gap-1 items-center justify-center">
+            <h1 className=" font-bold text-2xl">PRO</h1>
+            <p className="font-light">RS 29,90</p>
+          </div>
+
+          <ul className="my-auto flex flex-col gap-8  font-black text-center">
+            <li>
+              Entradas ilimitadas !
+            </li>
+            <li>
+              Saídas ilimitadas !
+            </li>
+            <li>
+              Ajuda do robo com seus gasto 🤖
+            </li>
+          </ul>
+
         </div>
 
-        <div className=" mt-14 flex flex-col gap-1 items-center justify-center">
-          <h1 className=" font-bold text-2xl">PRO</h1>
-          <p className="font-light">RS 29,90</p>
-        </div>
-
-        <ul className="my-auto flex flex-col gap-8  font-black text-center">
-          <li>
-            Entradas ilimitadas !
-          </li>
-          <li>
-            Saídas ilimitadas !
-          </li>
-          <li>
-            Ajuda do robo com seus gasto 🤖
-          </li>
-        </ul>
-
-      </div>
+      </a>
 
     </div>
 
