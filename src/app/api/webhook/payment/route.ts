c194eeb5a85668e5be4b68b86feae40532857e66
Nicolas Bonnefont <nicolas.7l@hotmail.com>
2023-07-18 17:05:37 -0300

    test webhook  stripe

--- src/app/api/webhook/payment/route.ts
+++ src/app/api/webhook/payment/route.ts
@@ -1,22 +1,23 @@
-import { NextApiRequest, NextApiResponse } from "next";
 
 const stripe = require('stripe')('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');
 
 // This is your Stripe CLI webhook secret for testing your endpoint locally.
 const endpointSecret = "whsec_ca93dae62cbaf0238344f2f9bf0f241b541a8d61be395a5860c68d482c60fa0d";
 
-export async function POST(request: NextApiRequest, response: NextApiResponse) {
+export async function POST(req: Request, res: Response) {
 
   try {
 
-    const sig = request.headers['stripe-signature'];
+    const sig = req.headers.get('stripe-signature');
 
     let event;
 
     try {
-      event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+      event = stripe.webhooks.constructEvent(req.body, sig, endpointSecret);
     } catch (err) {
-      response.status(400).send(`Webhook Error: ${err}`);
+      console.log(err)
+
+      /*   res.status(400).send(`Webhook Error: ${err}`); */
       return;
     }
 
@@ -24,9 +25,10 @@ export async function POST(request: NextApiRequest, response: NextApiResponse) {
     console.log(`Unhandled event type ${event.type}`);
 
     // Return a 200 response to acknowledge receipt of the event
-   
+
   } catch (error) {
     throw error
   }
 
 }
+
