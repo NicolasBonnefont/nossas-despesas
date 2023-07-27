83c749eff830a933ff80ef9b14b79ceb74987c31
Nicolas Bonnefont <nicolas.7l@hotmail.com>
2023-07-27 11:36:56 -0300

    fix: "a" to link Next

--- src/app/components/LandinPage/Precos/index.tsx
+++ src/app/components/LandinPage/Precos/index.tsx
@@ -1,37 +1,42 @@
+import Link from "next/link"
+
 function Precos() {
   return (
 
     <div id='preco' className="text-black flex flex-1 items-center justify-center w-full min-h-[60%] gap-8 max-md:flex-wrap py-24 bg-gray-900">
 
-      <div className="flex flex-col bg-white rounded-xl shadow-lg w-80  min-h-[500px] hover:bg-gray-200 cursor-pointer  " >
 
-        <div className=" flex items-center justify-center w-full h-10 bg-blue-900 rounded-b-xl rounded-t-lg">
-          <p className="text-yellow-200 font-bold">Uso Básico</p>
+      <Link href="/login">
+        <div className="flex flex-col bg-white rounded-xl shadow-lg w-80  min-h-[500px] hover:bg-gray-200 cursor-pointer  " >
+
+          <div className=" flex items-center justify-center w-full h-10 bg-blue-900 rounded-b-xl rounded-t-lg">
+            <p className="text-yellow-200 font-bold">Uso Básico</p>
+          </div>
+
+          <div className=" mt-14 flex flex-col gap-1 items-center justify-center">
+            <h1 className=" font-bold text-2xl">FREE</h1>
+            <p className="font-light">RS 0.00</p>
+          </div>
+
+          <ul className="my-auto flex flex-col gap-8  font-medium  text-center p-2">
+            <li>
+              Máximo de 10 entradas
+            </li>
+            <li>
+              Máximo de 10 saídas
+            </li>
+            <li>
+              Sem inteligência artificial
+            </li>
+            <li>
+              Sem relatório
+            </li>
+          </ul>
+
         </div>
+      </Link>
 
-        <div className=" mt-14 flex flex-col gap-1 items-center justify-center">
-          <h1 className=" font-bold text-2xl">FREE</h1>
-          <p className="font-light">RS 0.00</p>
-        </div>
-
-        <ul className="my-auto flex flex-col gap-8  font-medium  text-center p-2">
-          <li>
-            Máximo de 10 entradas
-          </li>
-          <li>
-            Máximo de 10 saídas
-          </li>
-          <li>
-            Sem inteligência artificial
-          </li>
-          <li>
-           Sem relatório
-          </li>
-        </ul>
-
-      </div>
-
-      <a target='blank' href="https://buy.stripe.com/eVa3g9dNIdkjguc5km">
+      <a href="https://buy.stripe.com/eVa3g9dNIdkjguc5km">
         <div className=" flex flex-col bg-white rounded-xl shadow-lg w-80  min-h-[500px] hover:bg-gray-200 cursor-pointer" >
 
           <div className=" flex items-center justify-center w-full h-10 bg-blue-900 rounded-b-xl rounded-t-lg">
